f56919fdf2558c1fb8db7d1ced1ad2341e6862e3
younis1431996 <61277970+younis1431996@users.noreply.github.com>
2021-05-22 16:44:11 +0200

    Create install_ipopt.sh

---
+++ install_ipopt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo chmod +x IPOPT.sh
+
+sudo apt-get install unzip
+wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.7.zip && unzip Ipopt-3.12.7.zip && rm Ipopt-3.12.7.zip
+sudo ./IPOPT.sh Ipopt-3.12.7
+
+sudo apt-get update
+sudo apt-get install lib32z1
